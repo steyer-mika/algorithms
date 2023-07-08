f53ea10beef722c6e17a8353ba9f5cf59ae918cb
Mika Steyer <mikasteyer9@gmail.com>
2023-07-08 15:09:42 +0200

    update binary tree test cases

--- src/binary-tree/binary-tree.spec.ts
+++ src/binary-tree/binary-tree.spec.ts
@@ -1,35 +1,33 @@
 import { BinaryTree } from "./binary-tree";
 
-describe("Binary Tree", () => {
-  it("should insert value", () => {
-    const tree = new BinaryTree<number>();
+describe("BinaryTree", () => {
+  let tree: BinaryTree<number>;
 
+  beforeEach(() => {
+    tree = new BinaryTree<number>();
+  });
+
+  it("should insert a value as the root", () => {
     tree.insert(5);
 
     expect(tree.root?.value).toEqual(5);
   });
 
-  it("should insert value left node", () => {
-    const tree = new BinaryTree<number>();
-
+  it("should insert a value as the left node", () => {
     tree.insert(5);
     tree.insert(2);
 
     expect(tree.root?.leftNode?.value).toEqual(2);
   });
 
-  it("should insert value right node", () => {
-    const tree = new BinaryTree<number>();
-
+  it("should insert a value as the right node", () => {
     tree.insert(5);
     tree.insert(7);
 
     expect(tree.root?.rightNode?.value).toEqual(7);
   });
 
-  it("should get min value", () => {
-    const tree = new BinaryTree<number>();
-
+  it("should get the minimum value", () => {
     tree.insert(5);
     tree.insert(2);
     tree.insert(8);
@@ -41,9 +39,7 @@ describe("Binary Tree", () => {
     expect(tree.getMin()).toEqual(2);
   });
 
-  it("should get max value", () => {
-    const tree = new BinaryTree<number>();
-
+  it("should get the maximum value", () => {
     tree.insert(5);
     tree.insert(2);
     tree.insert(8);
@@ -55,67 +51,43 @@ describe("Binary Tree", () => {
     expect(tree.getMax()).toEqual(9);
   });
 
-  it("should have value", () => {
-    const tree = new BinaryTree<number>();
-
+  it("should check if a value exists in the tree", () => {
     tree.insert(5);
     tree.insert(3);
     tree.insert(7);
 
     expect(tree.has(3)).toEqual(true);
-  });
-
-  it("should not have value", () => {
-    const tree = new BinaryTree<number>();
-
-    tree.insert(5);
-    tree.insert(3);
-    tree.insert(7);
-
     expect(tree.has(6)).toEqual(false);
   });
 
-  it("should remove root", () => {
-    const tree = new BinaryTree<number>();
-
+  it("should remove the root node", () => {
     tree.insert(5);
     tree.insert(4);
-
     tree.insert(3);
     tree.insert(2);
-    tree.remove(7);
+    tree.remove(5);
 
-    tree.remove(3);
-
-    expect(tree.has(3)).toEqual(false);
+    expect(tree.has(5)).toEqual(false);
   });
 
-  it("should remove root", () => {
-    const tree = new BinaryTree<number>();
-
+  it("should remove the root node and set a new root", () => {
     tree.insert(2);
-
     tree.remove(2);
 
     expect(tree.root).toEqual(null);
   });
 
-  it("should remove & set next root", () => {
-    const tree = new BinaryTree<number>();
-
+  it("should remove the root and set the next node as the new root", () => {
     tree.insert(5);
     tree.insert(2);
     tree.insert(7);
     tree.insert(8);
-
     tree.remove(5);
 
     expect(tree.root?.value).toEqual(7);
   });
 
-  it("should remove & replace next node", () => {
-    const tree = new BinaryTree<number>();
-
+  it("should remove a node and replace it with the next node in the inorder traversal", () => {
     tree.insert(8);
     tree.insert(3);
     tree.insert(14);
@@ -123,7 +95,6 @@ describe("Binary Tree", () => {
     tree.insert(1);
     tree.insert(4);
     tree.insert(7);
-
     tree.remove(3);
 
     expect(tree.root?.leftNode?.value).toEqual(4);
