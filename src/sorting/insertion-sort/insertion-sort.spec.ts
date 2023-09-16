d65830fc180da1f96db820095e99740b59b85f93
Mika Steyer <108517592+steyer-mika@users.noreply.github.com>
2023-09-16 19:06:18 +0200

    feat: add more test cases insertion sort

--- src/sorting/insertion-sort/insertion-sort.spec.ts
+++ src/sorting/insertion-sort/insertion-sort.spec.ts
@@ -13,9 +13,21 @@ describe('insertionSort', () => {
     expect(sortedArray).toEqual([42]);
   });
 
+  it('should handle an array with two elements', () => {
+    const twoElementsArray = [2, 1];
+    const sortedArray = insertionSort(twoElementsArray);
+    expect(sortedArray).toEqual([1, 2]);
+  });
+
   it('should sort an array with multiple elements', () => {
     const inputArray = [5, 3, 9, 1, 7];
     const sortedArray = insertionSort(inputArray);
     expect(sortedArray).toEqual([1, 3, 5, 7, 9]);
+  });
+
+  it('should not preserve the original array', () => {
+    const inputArray = [5, 3, 9, 1, 7];
+    const sortedArray = insertionSort(inputArray);
+    expect(inputArray).toEqual(sortedArray);
   });
 });
