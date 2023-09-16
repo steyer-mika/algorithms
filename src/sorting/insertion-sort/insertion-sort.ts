bdd919b7212c154c5cc0328098881be656eb92f9
Mika Steyer <108517592+steyer-mika@users.noreply.github.com>
2023-09-16 19:06:38 +0200

    feat: add comments insertion sort

--- src/sorting/insertion-sort/insertion-sort.ts
+++ src/sorting/insertion-sort/insertion-sort.ts
@@ -1,3 +1,13 @@
+/**
+ * Sorts the given array in ascending order using the Insertion Sort algorithm.
+ * This function mutates the original array.
+ *
+ * @param {T[]} array - The array to be sorted.
+ * @returns {T[]} - The sorted array.
+ *
+ * @template T
+ * @complexity O(n^2) - Where 'n' is the number of elements in the array.
+ */
 export const insertionSort = <T>(array: T[]): T[] => {
   for (let i = 1; i < array.length; i++) {
     const current = array[i];
